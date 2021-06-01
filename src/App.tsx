246bc7b8a2f822dd47e361d5bffede060b5e3d58
Cyklan <37187056+Cyklan@users.noreply.github.com>
2021-06-01 20:48:21 +0200

    Initial Commit
    Wallpaper, Task Bar, Clock, Start Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserView, MobileView } from "react-device-detect";
+import Desktop from "./pages/Desktop";
+import Mobile from "./pages/Mobile";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserView>
+        <Desktop />
+      </BrowserView>
+      <MobileView>
+        <Mobile />
+      </MobileView>
+    </>
   );
 }
 
---
+++ src/pages/Mobile.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Mobile() {
+  return <div className="mobile">coming soon<i>ish</i></div>;
+}
